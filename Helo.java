e800c0eddabd028ff4646879d6837db644157057
Manikanta Kharvi <126985671+maniw1233@users.noreply.github.com>
2025-04-19 03:05:39 +0530

    Update Helo.java

--- Helo.java
+++ Helo.java
@@ -2,5 +2,6 @@ class Helo{
     public static void main(String[] args) {
         System.out.println("Hello This is Java Code");
         System.out.println("what is Java");
+            System.out.println("what is Java");
     }
 }
